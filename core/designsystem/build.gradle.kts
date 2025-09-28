2f60d60d6a1ec1182e8b30af6fe62b5f7d1bdff8
Daniel sem <klutz@Daniels-MacBook-Pro.local>
2025-09-28 23:20:16 +0300

    Button style

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -9,6 +9,7 @@ kotlin {
                 implementation(libs.kotlin.stdlib)
 
                 implementation(compose.components.resources)
+                implementation(compose.components.uiToolingPreview)
             }
         }
         androidMain {

e057efe38475f7c76bed3c042649afb9b624e9b4
Daniel sem <klutz@Daniels-MacBook-Pro.local>
2025-10-20 20:27:10 +0300

    added open question

---
+++ feature/evaluation/presentation/src/commonMain/kotlin/maia/dmt/evaluation/presentation/renderers/InputQuestionRenderer.kt
@@ -0,0 +1,47 @@
+package maia.dmt.evaluation.presentation.renderers
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.foundation.text.input.setTextAndPlaceCursorAtEnd
+import androidx.compose.foundation.text.input.setTextAndSelectAll
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
+import maia.dmt.core.designsystem.components.textFields.DmtParagraphTextField
+import maia.dmt.core.domain.dto.evaluation.EvaluationObject
+import maia.dmt.evaluation.presentation.evaluation.EvaluationObjectType
+
+class InputQuestionRenderer : QuestionRenderer {
+    override fun canRender(objectType: Int): Boolean {
+        return EvaluationObjectType.fromInt(objectType) == EvaluationObjectType.INPUT
+    }
+
+    @Composable
+    override fun Render(
+        question: EvaluationObject,
+        currentAnswer: String,
+        onAnswerChange: (String) -> Unit
+    ) {
+
+        val state = rememberTextFieldState(initialText = currentAnswer)
+
+        val currentText = state.text.toString()
+        LaunchedEffect(currentText) {
+            if (currentText != currentAnswer) {
+                onAnswerChange(currentText)
+            }
+        }
+
+        LaunchedEffect(currentAnswer) {
+            if (currentAnswer != state.text.toString()) {
+                state.setTextAndPlaceCursorAtEnd(currentText)
+            }
+        }
+
+        DmtParagraphTextField(
+            state = state,
+            modifier = Modifier.fillMaxWidth(),
+            placeholder = question.object_label
+        )
+    }
+}
